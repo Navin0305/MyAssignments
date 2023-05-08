53d09f0b13c275c896b6c15ecdedc37dcbcad466
Navin0305 <132676613+Navin0305@users.noreply.github.com>
2023-05-08 22:16:36 +0530

    Add files via upload

---
+++ day2/Mobile.java
@@ -0,0 +1,23 @@
+package week1.day2;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		String mobileBrandName= "Redmi";
+		System.out.println(mobileBrandName);
+		char mobileLogo='R';
+		System.out.println(mobileLogo);
+		short noOfMobilePiece=1;
+		System.out.println(noOfMobilePiece);
+		int modelNumber=8;
+		System.out.println(modelNumber);
+		long mobileImeiNumber=78967123L;
+		System.out.println(mobileImeiNumber);
+		float mobilePrice=19999.9F;
+		System.out.println(mobilePrice);
+		boolean isDamaged=false;
+		System.out.println(isDamaged);
+
+	}
+
+}
---
+++ day2/TwoWheeler.java
@@ -0,0 +1,23 @@
+package week1.day2;
+
+public class TwoWheeler {
+
+	public static void main(String[] args) {
+		int noOfWheels=4;
+		System.out.println(noOfWheels);
+		short noOfMirrors=3;
+		System.out.println(noOfMirrors);
+		long chasisNumber=9879879L;
+		System.out.println(chasisNumber);
+		boolean isPunctured= false;
+		System.out.println(isPunctured);
+		String bikeName="KTM RC200";
+		System.out.println(bikeName);
+		double runningKM=20037.8;
+		System.out.println(runningKM);
+		
+
+	}
+
+}
+
